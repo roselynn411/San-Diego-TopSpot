73f4350c96d4b6a962b771081fc6573517ba9758
Roselynn <worklaptop@Roselynns-MacBook-Pro.local>
2022-03-14 10:36:54 -0400

    second ccommit

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+//Page can't be manipulated until document is ready
+// jQuery detects this state of readiness for you
 $(document).ready(function(){
+  //getting data from data.json
 $.getJSON("data.json", function(data){
 //log all data
-
+//grab table and saved it in table var
 let table = document.getElementById('table');
 //create loop to iterate through each element in array
 //grab the name from object and add to first id cell
@@ -15,8 +18,10 @@ for(let i = 0; i < data.length ; i++){
   let cell1 = newRow.insertCell(0);
   let cell2 = newRow.insertCell(1);
   let cell3 = newRow.insertCell(2);
+  let cell4 = newRow.insertCell(3);
 
  //add name cells
+ //create text node and the data : string containing the data to be put in the text node 
  let cell1Text = document.createTextNode(data[i].name);
  cell1.appendChild(cell1Text);
 
